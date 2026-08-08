8840d7d70216190e391fff554de500af2559ec3b
tihaya-anon <chihaya_anon_0908@outlook.com>
2026-08-08 14:16:06 +0800

    744: submit

---
+++ binary-search/744.py
@@ -0,0 +1,52 @@
+"""
+744. Find Smallest Letter Greater Than Target
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+Example 3:
+
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+
+
+Constraints:
+
+2 <= letters.length <= 104
+letters[i] is a lowercase English letter.
+letters is sorted in non-decreasing order.
+letters contains at least two different characters.
+target is a lowercase English letter.
+"""
+
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        L, R = -1, len(letters)
+        while R - L > 1:
+            md = R + L >> 1
+            if letters[md] <= target:
+                L = md
+            else:
+                R = md
+        return letters[R] if R != len(letters) else letters[0]
