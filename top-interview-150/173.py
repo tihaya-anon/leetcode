60986a96c77b0c656a44f532a0b4eae819a8cee9
luoyx <luoyx@shokz.com.cn>
2026-08-19 18:19:39 +0800

    tmp

---
+++ top-interview-150/173.py
@@ -0,0 +1,38 @@
+from typing import List, Optional
+
+null = None
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    @classmethod
+    def build_from_list(cls, li: List) -> Optional["TreeNode"]:
+        for i, e in enumerate(li):
+            if e is None:
+                continue
+            li[i] = cls(e)
+            if i == 0:
+                continue
+            parent = i - 1
+            parent //= 2
+            if i % 2:
+                li[parent].left = li[i]
+            else:
+                li[parent].right = li[i]
+        return li[0]
+
+
+class BSTIterator:
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.cur = root
+
+    def next(self) -> int:
+        return 0
+
+    def hasNext(self) -> bool:
+        return False
